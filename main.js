75baefbe208399c5a4d884ab6dfd505036f9ea10
Doston Norpulatov <norpulatovdoston@gmail.com>
2018-08-06 13:29:41 +0500

    menu fixed

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Up button
+
 $('body').append('<button class="btn_up"></button>');
 
 $('.btn_up').click(function() {
@@ -16,3 +18,37 @@ $(window).scroll(function() {
 		$('.btn_up').removeClass('btn_up-active');
 	}
 });
+
+// 
+
+// Fixed menu
+
+$(document).ready(function() {
+ 
+var navPos, winPos, navHeight;
+  
+function refreshVar() {
+	navPos = $('nav').offset().top;
+	navHeight = $('nav').outerHeight(true);
+}
+
+refreshVar();
+$(window).resize(refreshVar);
+	
+	$('<div class="clone-nav"></div>').insertBefore('nav').css('height', navHeight).hide();
+  
+$(window).scroll(function() {
+	winPos = $(window).scrollTop();
+
+	if (winPos >= navPos) {
+		$('nav').addClass('fixed-menu');  
+		$('.clone-nav').show();
+	} else {
+		$('nav').removeClass('fixed-menu');
+		$('.clone-nav').hide();
+	}
+});
+
+});
+
+// 
