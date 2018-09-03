ee057ba2c63190393a773c109e9fe2982fd31d19
Doston Norpulatov <norpulatovdoston@gmail.com>
2018-09-03 11:21:28 +0500

    added english version

--- portfolio/converter/main.js
+++ portfolio/converter/main.js
@@ -1,5 +1,6 @@
-var uzs = document.getElementById('uzs');
-var usd = document.getElementById('usd');
+var uzs = document.getElementById('uzs'),
+	usd = document.getElementById('usd'),
+	curse = 8005;
 
 uzs.oninput = function () {
 	usd.value = uzs.value / 8005;
@@ -15,12 +16,12 @@ usd.oninput = function () {
 	}
 }
 
-// function convert (valut1, valut2, curse){
+// function convert (valut1, valut2){
 // 	valut1.value = valut2.value * curse;
 // 	if (isNaN(valut1)) {
 // 		valut1.value = "Ensert a number";
 // 	}
 // }
 
-// uzs.onkeypress = convert (usd, uzs, 0.00125);
-// usd.onkeypress = convert (uzs, usd, 8030);
+// uzs.oninput =  function convert (usd, uzs);
+// usd.oninput = function convert (uzs, usd);
