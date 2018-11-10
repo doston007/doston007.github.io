4f42c612b59414be78b09caf568c21528f069de0
Doston Norpulatov <norpulatovdoston@gmail.com>
2018-11-11 00:51:10 +0500

    job

--- job/app/js/common.js
+++ job/app/js/common.js
@@ -3,8 +3,10 @@ $(function() {
 	// fullpage
 	$('#fullpage').fullpage({
 		licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-		anchors: ['main', 'regulations'],
-		menu: '#top_nav'
+		menu: '#top_nav',
+		navigation: true,
+		navigationPosition: 'right',
+		showActiveTooltip: false,
 	});
 
 	$('#clock').countdown('2018/11/24', function(event) {
