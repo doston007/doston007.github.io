ac1e0d90131609ac54e8eec2ea2978bb06ecb795
Doston Norpulatov <norpulatovdoston@gmail.com>
2019-09-02 16:53:12 +0500

    job

---
+++ mms/js/common.js
@@ -0,0 +1,48 @@
+window.addEventListener('load', function() {
+
+	// FullPage
+	let sections = document.querySelectorAll('.section');
+	let sectionAnchors = [];
+
+	sections.forEach(function(section) {
+		let anchor = section.getAttribute('data-section');
+		sectionAnchors.push(anchor + "");
+	});
+
+	$('#fullpage').fullpage({
+		menu: '#main-nav',
+		navigation: false,
+		anchors: sectionAnchors
+	});
+
+	$('#js-to-down').on('click', function () {
+		try {
+			// fullpage scroll
+			$.fn.fullpage.moveSectionDown();
+		} catch (error) {
+			// normal scroll
+			$root.animate({
+				scrollTop: window.innerHeight
+			}, 400, function () {
+			});
+		}
+		
+	});
+
+
+	// Slick slider
+	$('#js-slider-img').slick({
+		dots: false,
+		arrows: true,
+		slidesToShow: 2,
+		slidesToScroll: 1
+	});
+
+	$('#js-product-slider').slick({
+		dots: false,
+		arrows: true,
+		slidesToShow: 4,
+		slidesToScroll: 1
+	})
+
+});
