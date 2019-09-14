9268e031521936eddd575f2c2c949609a8895935
Doston Norpulatov <norpulatovdoston@gmail.com>
2019-09-14 16:32:39 +0500

    job

--- mms/js/common.js
+++ mms/js/common.js
@@ -27,22 +27,29 @@ window.addEventListener('load', function() {
 		}
 	});
 
-	$('#js-to-down').on('click', function () {
-		try {
-			// fullpage scroll
-			$.fn.fullpage.moveSectionDown();
-		} catch (error) {
-			// normal scroll
-			$root.animate({
-				scrollTop: window.innerHeight
-			}, 400, function () {
-			});
-		}
+	// $('#js-to-down').on('click', function () {
+	// 	try {
+	// 		// fullpage scroll
+	// 		$.fn.fullpage.moveSectionDown();
+	// 	} catch (error) {
+	// 		// normal scroll
+	// 		$root.animate({
+	// 			scrollTop: window.innerHeight
+	// 		}, 400, function () {
+	// 		});
+	// 	}
 		
+	// });
+
+
+	// Slick sliders
+	$('#js-hero-slider').slick({
+		dots: false,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 5000,
 	});
 
-
-	// Slick slider
 	$('#js-slider-img').slick({
 		dots: false,
 		arrows: true,
@@ -55,6 +62,76 @@ window.addEventListener('load', function() {
 		arrows: true,
 		slidesToShow: 4,
 		slidesToScroll: 1
-	})
+	});
+
+	// More button
+	$('.hero-section__button').on('click', function(event) {
+		event.preventDefault();
+		try {
+			// fullpage scroll
+			$.fn.fullpage.moveSectionDown();
+		} catch (error) {
+			// normal scroll
+			$root.animate({
+				scrollTop: window.innerHeight
+			}, 400, function () {
+			});
+		}
+	});
+
+	// About Product Building
+	let details = [
+		{
+			detailNumber: 2331,
+			detailImgPath: 'img/motor-main.png',
+			detailDescription: 'ПРИМЕР ПЕРВОЙ ПРОДУКЦИИ unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?' 
+		},
+		{
+			detailNumber: 2332,
+			detailImgPath: 'img/motor-main.png',
+			detailDescription: 'ПРИМЕР ВТОРОЙ ПРОДУКЦИИSed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?' 
+		},
+		{
+			detailNumber: 2333,
+			detailImgPath: 'img/motor-main.png',
+			detailDescription: 'ПРИМЕР ТРЕТЬЕЙ ПРОДУКЦИИ Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?' 
+		},
+		{
+			detailNumber: 2334,
+			detailImgPath: 'img/motor-main.png',
+			detailDescription: 'ПРИМЕР ЧЕТВЕРТОЙ ПРОДУКЦИИ Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?' 
+		}
+	];
+
+	$('#js-detail-img').attr('src', details[0].detailImgPath);
+	$('#js-detail-number').html(details[0].detailNumber);
+	$('#js-detail-description').html(details[0].detailDescription);
+
+	let productCard = $('.product-card');
+	let detailNum;
+
+	productCard.on('click', function(event) {
+		event.preventDefault();
+		detailNum = $(this).attr('data-detail-number');
+
+		for (let i = 0; i < details.length; i++) {
+			if (details[i].detailNumber == detailNum) {
+				$('#js-detail-img').attr('src', details[i].detailImgPath);
+				$('#js-detail-number').html(details[i].detailNumber);
+				$('#js-detail-description').html(details[i].detailDescription);
+			}
+		}
+
+		try {
+			// fullpage scroll
+			$.fn.fullpage.moveSectionDown();
+		} catch (error) {
+			// normal scroll
+			$root.animate({
+				scrollTop: window.innerHeight
+			}, 400, function () {
+			});
+		}
+	});
 
 });
